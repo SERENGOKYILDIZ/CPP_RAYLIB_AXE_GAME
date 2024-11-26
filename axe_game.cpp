f2ea753a4486dfcfd685b4d449ad691795bcb78b
Semi Eren Gokyildiz <gokyildizsemieren@gmail.com>
2024-11-26 16:56:45 +0300

    Made Data types and variables

--- axe_game.cpp
+++ axe_game.cpp
@@ -10,5 +10,12 @@ by Eren
 int main()
 {
     //Creating a popup window
-    InitWindow(1000, 300, "AXE GAME");
+    int width = 1200;
+    int height = 800;
+    InitWindow(width, height, "AXE GAME");
+
+    //Data Types and Variables
+    float root_beer = 1.99;
+    double cheese_burger = 5.99;
+    bool shouldHaveLunch;
 }
