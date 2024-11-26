6fb45ae95ec220edf72f3c6a14ba012d5f5b8619
Semi Eren Gokyildiz <gokyildizsemieren@gmail.com>
2024-11-26 17:01:43 +0300

    Made Comparison Operators

--- axe_game.cpp
+++ axe_game.cpp
@@ -18,4 +18,13 @@ int main()
     float root_beer = 1.99;
     double cheese_burger = 5.99;
     bool shouldHaveLunch;
+
+    //Comparison Operators
+    bool equal{4==9};
+    bool notEqual{4!=9};
+    bool less{4<9};
+    bool greater{4>9};
+    bool lessEqual{4<=9};
+    bool greaterEqual{4>=9};
+
 }
